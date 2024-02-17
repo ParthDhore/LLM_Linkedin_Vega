419e5990ce161a59f090a2e5128e7ea9bcfe72e7
ParthDhore <parthdhore14@gmail.com>
2024-02-17 15:28:55 +0530

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from langchain.llms.huggingface_hub import HuggingFaceHub
 import os
-from getpass import getpass
 from dotenv import load_dotenv
 
 
@@ -13,16 +12,16 @@ llm = HuggingFaceHub(
 
 query = "What is langchain?"
 
-# prompt = f"""
-#  <|system|>
-# You are an AI assistant that follows instruction extremely well.
-# Please be truthful and give direct answers
-# </s>
-#  <|user|>
-#  {query}
-#  </s>
-#  <|assistant|>
-# """
+prompt = f"""
+ <|system|>
+You are an AI assistant that follows instruction extremely well.
+Please be truthful and give direct answers
+</s>
+ <|user|>
+ {query}
+ </s>
+ <|assistant|>
+"""
 
-response = llm.predict(query)
+response = llm.predict(prompt)
 print(response)
